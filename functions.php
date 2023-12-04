d770f8e32851d457fa5f3a5005a83c9a52b4ab4c
Can Ata <canata09074@gmail.com>
2023-12-04 18:49:48 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -9,16 +9,14 @@
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 
 // Define your custom function
-function my_custom_content_wrapper_start() {
-    echo '<div id="my-custom-content-wrapper">'; // Replace this with your custom HTML
+function woocommerce_output_content_wrapper() {
+    if ( is_post_type_archive( 'product' ) ) {
+        wc_get_template( 'archive-product.php' );
+    }
 }
 
 // Add your custom function to the action hook
-add_action( 'woocommerce_before_main_content', 'my_custom_content_wrapper_start', 10 );
-
-
-
-
+add_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 5 );
 
 
 /**
@@ -31,37 +29,25 @@ remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wra
 
 
 
-/**
- * Remove the default WooCommerce content end wrapper
- *
- *
-
-// In your theme's functions.php file
-remove_all_actions( 'woocommerce_before_main_content' );
-
-add_action( 'woocommerce_before_main_content', 'custom_archive_product_content', 5 );
-function custom_archive_product_content() {
-    if ( is_post_type_archive( 'product' ) ) {
-        wc_get_template( 'archive-product.php' );
-    }
-}
-*/
-
-
 
 
 
 /**
- * Remove the default WooCommerce content end wrapper
+ * Remove the before single product
  *
- *
-add_action( 'woocommerce_before_single_product', 'custom_single_product_content', 5 );
-function custom_single_product_content() {
+ */
+remove_action( 'woocommerce_before_single_product', 'woocommerce_output_content_wrapper', 10 );
+
+// Define your custom function
+function woocommerce_output_content_wrapper() {
     if ( is_product() ) {
         wc_get_template( 'single-product.php' );
     }
 }
-*/
+
+// Add your custom function to the action hook
+add_action( 'woocommerce_before_single_product', 'woocommerce_output_content_wrapper', 5 );
+
 
 
 
@@ -81,12 +67,6 @@ function woocommerce_output_content_wrapper() {
 
 // Add your custom function to the action hook
 add_action( 'woocommerce_before_cart', 'woocommerce_output_content_wrapper', 5 );
-
-
-
-
-
-
 
 
 /**
@@ -118,12 +98,6 @@ function woocommerce_output_content_wrapper() {
 
 // Add your custom function to the action hook
 add_action( 'woocommerce_before_checkout_form', 'woocommerce_output_content_wrapper', 5 );
-
-
-
-
-
-
 
 
 /**
